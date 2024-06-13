be6a4d49edbfa1d9ad2d4e3b6ac90936e0ecc719
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-06-13 17:05:23 -0300

    feat: extract markdown images function + tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,12 @@
 from htmlnode import LeafNode
 
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -14,21 +21,22 @@ class TextNode:
         )
         
         
-    def to_html(self):
-        text_type = self.text_type
-        if text_type == "text":
-            return LeafNode(tag=None, value=self.text).to_html()
-        elif text_type == "bold":
-            return LeafNode(tag="b", value=self.text).to_html()
-        elif text_type == "italic":
-            return LeafNode(tag="i", value=self.text).to_html()
-        elif text_type == "code":
-            return LeafNode(tag="code", value=self.text).to_html()
-        elif text_type == "link":
-            return LeafNode(tag="a", value=self.text, props={"href": self.url}).to_html()
-        elif text_type == "image":
-            return LeafNode(tag="img", value="", props={"src": self.url, "alt": self.text}).to_html()
-              
+    def to_html(text_node):
+        if text_node.text_type == text_type_text:
+            return LeafNode(None, text_node.text).to_html()
+        if text_node.text_type == text_type_bold:
+            return LeafNode("b", text_node.text).to_html()
+        if text_node.text_type == text_type_italic:
+            return LeafNode("i", text_node.text).to_html()
+        if text_node.text_type == text_type_code:
+            return LeafNode("code", text_node.text).to_html()
+        if text_node.text_type == text_type_link:
+            return LeafNode("a", text_node.text, {"href": text_node.url}).to_html()
+        if text_node.text_type == text_type_image:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text}).to_html()
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
+        
+        
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
