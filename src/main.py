0d2c04471e8a3afbcd5ef26b005f49cb4e6ac0a2
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-06-14 01:22:41 -0300

    feat: recursevily copying files.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
-from textnode import TextNode
+import os
+import shutil
+from copy_dir import copy_files_recursive
+
+dir_path_public = "./public"
+dir_path_static = "./static"
 
 def main():
-    textnode1 = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(textnode1)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    
+    print("Copying static directory to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+        
+    return
     
 if __name__ == "__main__":
     main()
