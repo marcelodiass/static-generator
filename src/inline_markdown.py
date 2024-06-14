13b20ce07b279142b04bb4c16981bc023c184a19
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-06-13 23:30:48 -0300

    feat: text_to_nodes function + tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -9,6 +9,15 @@ text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
 
+def text_to_nodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+    
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes = []
         for old_node in old_nodes:
